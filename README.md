5d72c76223566608d5ae066dfedf92d695d85d5e
Akopalips <48akopalips@gmail.com>
2019-07-11 15:01:03 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-bqtsgt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-bqtsgt)
---
+++ src/app/tus-map.component.ts
@@ -0,0 +1,44 @@
+
+import { Component, OnInit } from '@angular/core';
+import 'leaflet';
+declare let L;
+
+@Component({
+  selector: 'TusMap',
+  templateUrl: './tus-map.component.html',
+  styleUrls: ['./tus-map.component.css']
+})
+
+export class TusMap implements OnInit {
+  constructor() {}
+  map;
+  marker;
+
+  //ngOnInit вызывается сразу после того, как свойства, привязанные к данным, были проверены в первый раз и до того, как был проверен любой из его дочерних элементов. Он вызывается только один раз, когда директива создается.
+  ngOnInit() {
+    const map = L.map('Leaflet-Map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoidHVzcHJpbmciLCJhIjoiY2p4dTdsOW54MDJ3ejNtcDk5czFjMWYyOSJ9.Tp41AghE2hkJ3mRnn023tA'
+    }).addTo(map);
+
+
+      
+    this.marker = 123
+
+    this.map = map.getCenter()
+    
+    function onMapClick(e) {
+      L.marker(e.latlng).addTo(map);
+      this.marker = e.latlng
+    }
+    function onMapMoved(e) {
+      this.this.map = map.getCenter()
+    }
+
+
+ 
+    map.on('click', onMapClick);
+    //map.on('clmousemoveick', onMapMoved);
+  }
+}
